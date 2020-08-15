d2870084df4e52d1bab0af0b801c938859c0e787
massu116 <kakeru1999116@yahoo.co.jp>
2020-08-15 16:25:14 +0900

    おかえりI.D.追加

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var label: UILabel!
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -9,8 +10,17 @@ class ViewController: UIViewController {
         label.text = "Hello World !!!!"
         label.font = label.font.withSize(40)
         
+        button.setTitle("ただいま", for: .normal)
+        
+        let buttonSize = UIFont.systemFont(ofSize: 30)
+        
+        button.titleLabel?.font = buttonSize
+        
     }
 
 
+    @IBAction func tapbutton(_ sender: Any) {
+        label.text = "おかえりI.D."
+    }
 }
 

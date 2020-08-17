b463c86283c50274a17fa162de23f67cff94f59a
massu116 <kakeru1999116@yahoo.co.jp>
2020-08-18 05:12:19 +0900

    I.D.ボイス追加

--- IDmovie/IDmovie/NextViewController.swift
+++ IDmovie/IDmovie/NextViewController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class NextViewController: UIViewController {
 
+    @IBOutlet weak var startbutton: UIButton!
     @IBOutlet weak var nextLabel: UILabel!
+    
+    let path = Bundle.main.bundleURL.appendingPathComponent("id.mp3")
+    var musicPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        nextLabel.text = "遷移したでー"
+        startbutton.setTitle("start", for: .normal)
+        
+        let buttonSize = UIFont.systemFont(ofSize: 30)
+        startbutton.titleLabel?.font = buttonSize
         // Do any additional setup after loading the view.
     }
     
 
+    @IBAction func startbutton(_ sender: Any) {
+        do{
+            musicPlayer = try AVAudioPlayer(contentsOf: path)
+            musicPlayer.play()
+        }catch{
+            print("error")
+        }
+    }
     /*
     // MARK: - Navigation
 

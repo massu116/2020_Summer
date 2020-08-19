79367082c489bde15bb4e85e983dc22024394187
massu116 <kakeru1999116@yahoo.co.jp>
2020-08-20 01:35:32 +0900

    NO use storyboad

---
+++ noUseStoryboad/noUseStoryboad/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  noUseStoryboad
+//
+//  Created by massu_fun on 2020/08/20.
+//  Copyright © 2020 massu_fun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //UILabelをインスタンス化
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //ラベルテキストの設定
+        label.text = "おかえり"
+        //テキストのカラーを設定
+        label.textColor = .white
+        //背景色の設定
+        label.backgroundColor = .black
+        //場所と大きさの設定
+        label.frame = CGRect(x: 50, y: 50, width: 300, height: 100)
+        //ラベルの角を丸める
+        label.layer.cornerRadius = 20
+        label.clipsToBounds = true
+        //ラベルテキストをセンター寄せ
+        label.textAlignment = NSTextAlignment.center
+        label.font = UIFont.systemFont(ofSize: 30)
+        //ラベルをビューに追加
+        self.view.addSubview(label)
+    }
+
+
+}
+
---
+++ text/text/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  text
+//
+//  Created by massu_fun on 2020/08/18.
+//  Copyright © 2020 massu_fun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func number(_ sender: Any) {
+    }
+    
+
+}
+
